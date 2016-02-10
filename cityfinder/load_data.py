2e46132deb3adda90bf39442ec143c48166ca458
Dani <dani@Maxs-iMac.local>
2016-02-09 21:00:42 -0600

    working on loading data- changed models - need to write func to input all data and get migrate to work

---
+++ cityfinder/load_data.py
@@ -0,0 +1,47 @@
+from django.db import connections
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.utils import ConnectionDoesNotExist    
+from cityfinderapp import models
+
+'''
+Inspired by: 
+http://jantoniomartin.tumblr.com/post/15233766067/django-how-to-import-data-from-an-external
+'''
+
+def setup_cursor():
+    try:
+        return connections['default'].cursor()
+    except ConnectionDoesNotExist:
+        print "Legacy database is not configured"
+        return
+
+def import_walk():
+    cursor = setup_cursor()
+    if cursor is None:
+        print('no cursor')
+        return
+    sql = """SELECT * FROM walk"""
+    cursor.execute(sql)
+    for row in cursor.fetchall():
+        walk = models.Walk(city=row[0], state=row[1], walk_score=row[2],
+            transit_score=row[3], bike_score=row[4], population=row[5])
+        walk.save()
+
+def import_weather():
+    cursor = setup_cursor()
+    if cursor is None:
+        print('no cursor')
+        return
+    sql = """SELECT * FROM weather"""
+    cursor.execute(sql)
+    for row in cursor.fetchall():
+        weather = models.Weather(city=row[0], state=row[1], avg_temp_jan=row[2],
+            avg_temp_april=row[3], avg_temp_july=row[4], avg_temp_oct=row[5],
+            avg_annual_precip_in=row[6],avg_annual_precip_in_days=row[7], 
+            avg_annual_precip_snowfall=row[8])
+        weather.save()
+
+# import_walk()
+import_weather()
+
