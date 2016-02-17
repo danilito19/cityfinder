3f0d604b808f98893629f8cd64ba2d085bd5abda
Dana Alcala <Dani@Danas-MacBook-Air.local>
2016-02-17 10:06:27 -0600

    need to make priorities work

--- cityfinder/cityfinderapp/urls.py
+++ cityfinder/cityfinderapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.preferences, name='preferences'),
+    url(r'^priorities/$', views.priorities, name='priorities'),
 ]
--- cityfinder/cityfinderapp/views.py
+++ cityfinder/cityfinderapp/views.py
@@ -3,9 +3,12 @@ from django.template import RequestContext
 from django.http import HttpResponse
 from .models import *
 
-def home(request):
+def preferences(request):
     walk_city = Walk.objects.order_by('-city')[:50]
-    #output = ', '.join([str(q) for q in walk_city])
-    return render(request, 'base.html', {'cities': walk_city})
+    return render(request, 'preferences.html', {'cities': walk_city})
+
+def priorities(requst):
+    walk_city = Walk.objects.order_by('-city')[:50]
+    return render(request, 'priorities.html', {'cities': walk_city})
 
 #each view has to return an httpresponse or exception
