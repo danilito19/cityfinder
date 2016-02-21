7537e045bb2c9c3313dde827a8d40ff72fae551f
annalizhaz <annalizhaz@gmail.com>
2016-02-20 22:19:29 -0600

    Fixing filepaths to js and css, experiments with forms

--- cityfinder/cityfinderapp/static/js/priorities.js
+++ cityfinder/cityfinderapp/static/js/priorities.js
@@ -12,3 +12,19 @@ Sortable.create(sortFalse, {
   sort: false
 });
 
+function get_inputs() {
+	var priority_block = document.getElementById("sortTrue");
+	priority_list = []
+	for(i = 1; i < priority_block.childNodes.length; i++){
+		list_item = priority_block.childNodes[i];
+		priority_list.push(list_item.id)
+	}
+	document.getElementById("priorities").value = priority_list;
+
+	//console.log things are just checks for now printed to console, can be deleted in final
+	console.log(priority_list);
+	console.log(Array.isArray(priority_list))
+	
+	//document.getElementById("priorities").submit();
+	//window.location.href = "preferences.html"
+}
