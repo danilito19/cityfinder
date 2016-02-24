ba85dafa8aaf7d096e1b6f2da0f59b1ef99e3677
Dani <dani@Maxs-iMac.local>
2016-02-23 20:08:36 -0600

    working javascript and now chosen priorities are carried thru to preferences page to be used

--- cityfinder/cityfinderapp/views.py
+++ cityfinder/cityfinderapp/views.py
@@ -6,24 +6,35 @@ from .models import *
 #keep this code for example
 #  walk_city = Walk.objects.order_by('-city')[:50]
 
-def preferences(request):
-  template = loader.get_template('preferences.html')
-  return render(request, 'preferences.html')
-  # return HttpResponse(template.render(context, request))
-
 def priorities(request):
-  template = loader.get_template('priorities.html')
-  # context = {
-  #     'test_context': 'hellow!',
-  #   }
-  # if request.session.test_cookie_worked():
-  #   request.session.delete_test_cookie()
-  #   return HttpResponse("You're logged in.")
-  # else:
-  #   return HttpResponse("Please enable cookies and try again.")
+  # All this happens before any HTML is sent to the browser
   request.session.set_test_cookie()
-    
-  return HttpResponse(template.render(request))
+  if request.session.test_cookie_worked():
+    request.session.delete_test_cookie()
+  else:
+    return HttpResponse("Please enable cookies and try again.")
+  
+  # this enables us to show any previous selections if user went back
+  # to the first page. Otherwise no harm done
+  context = {'previous_priorities': []}
+  if request.session['priorities']:
+    for priority in request.session['priorities']:
+      context['previous_priorities'].append(priority)
+
+  # render HTML for browser here
+  return render(request, 'priorities.html', context)
+
+
+def preferences(request):
+  # we get priorities from previous page via request.POST
+  print request.POST
+  # we now save these priorities for later use, or if user goes back
+  request.session['priorities'] = request.POST['priorities']
+  # stored_variable = request.session['test']
+  # return render(request, 'preferences.html', {'test_context': stored_variable})
+  return render(request, 'preferences.html')
+
+
 
 
 #each view has to return an httpresponse or exception
