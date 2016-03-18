dcf4558e00aa2c6c5858c86ba63b5d6396ea7d85
annalizhaz <annalizhaz@gmail.com>
2016-03-18 00:23:39 -0500

    Fixed for Firefox

--- cityfinder/cityfinderapp/static/js/priorities.js
+++ cityfinder/cityfinderapp/static/js/priorities.js
@@ -19,17 +19,17 @@ to their priority list upon clicking the "continue" button and returns this
 ordered list to the back end. The user is then moved to the next page.
 */
 
-$('#arrow').on("click", function(e){
+$('#bottom-button').click(function(e){
     e.preventDefault();
 	var priority_block = document.getElementById("sortTrue");
 	var priority_list = [];
 
 	for(var i = 1; i < priority_block.childNodes.length; i++){
 		list_item = priority_block.childNodes[i];
-		priority_list.push(list_item.id)
-	}
+		priority_list.push(list_item.id);
+	};
 
-	console.log(priority_list)
+	console.log(priority_list);
 
 	document.getElementById('priorities').value = priority_list;
 	$('#priorities-form').attr('action', "./preferences_citysize/").submit();
